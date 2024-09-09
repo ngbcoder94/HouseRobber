1530cc43965c958609714c95b5464927807652a1
ngbcoder94 <ngbcoder94@gmail.com>
2024-09-08 21:32:11 -0400

    Solution created

--- HouseRobber.java
+++ HouseRobber.java
@@ -14,7 +14,19 @@ public class HouseRobber{
                 if(nums.length == 0) return 0; 
                 if(nums.length ==  1) return nums[0];
 
+                //Now I need to create an array to store the max money that can be robbed/house
+                int[] dp = new int[nums.length];
 
+                //Base cases
+                dp[0] = nums[0];    //Rob the first house
+                dp[1] = Math.max(nums[0], nums[1]);  //Rob the house that has more
+                
+                //Fill the dp array
+                for(int i = 2; i < nums.length; i++){
+                        dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i]);
+                }
+
+                //The last entry in the array will have the max value.
+                return dp[nums.length - 1];
         }
-        
 }
